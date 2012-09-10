caf5eadceb36fb85daf24b1e6796f30cd0eb0f20
Asko Soukka <asko.soukka@iki.fi>
2012-09-10 22:58:18 +0300

    Refactored setup.py. Renamed HISTORY.txt to CHANGES.txt.

--- setup.py
+++ setup.py
@@ -2,35 +2,35 @@ from setuptools import setup, find_packages
 
 version = "0.6.0"
 
-requires=[
-    "setuptools",
-    "unittest2",
-    "robotframework>=2.7.1",
-    ]
 
-setup(name="robotsuite",
-      version=version,
-      description="Robot Framework test suite for Python unittest framework",
-      long_description=open("README.rst").read() + "\n" +
-                       open("HISTORY.txt").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      keywords="",
-      author="Asko Soukka",
-      author_email="asko.soukka@iki.fi",
-      url="https://github.com/datakurre/robotsuite/",
-      license="GPL",
-      packages=find_packages("src", exclude=["ez_setup"]),
-      package_dir={"": "src"},
-      namespace_packages=[],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      extras_require={
-      },
-      entry_points="""
-      """
-      )
+setup(
+    name="robotsuite",
+    version=version,
+    description="Robot Framework test suite for Python unittest framework",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords="",
+    author="Asko Soukka",
+    author_email="asko.soukka@iki.fi",
+    url="https://github.com/collective/robotsuite/",
+    license="GPL",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    package_dir={"": "src"},
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "unittest2",
+        "robotframework>=2.7.1",
+    ],
+    extras_require={"test": [
+        "plone.app.testing",
+        "robotframework-selenium2library",
+    ]}
+)
