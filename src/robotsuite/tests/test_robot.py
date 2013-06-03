c742a8222f7cf007bf4e30e93b930e8706652376
Asko Soukka <asko.soukka@iki.fi>
2013-06-03 20:17:25 +0300

    Fix Plone test example to follow p.a.robotframework practices (and use compatible variables)

--- src/robotsuite/tests/test_robot.py
+++ src/robotsuite/tests/test_robot.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-import robotsuite
 from plone.app.testing import PLONE_ZSERVER
 from plone.testing import layered
+
+import robotsuite
 
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("robot_plone_login.txt"),
+        layered(robotsuite.RobotTestSuite("test_plone_login.robot"),
                 layer=PLONE_ZSERVER),
     ])
     return suite
-
-
-class Keywords(object):
-    """An example Robot Framework python keyword library"""
-
-    def get_test_user_name(self):
-        import plone.app.testing
-        return plone.app.testing.interfaces.TEST_USER_NAME
-
-    def get_test_user_password(self):
-        import plone.app.testing
-        return plone.app.testing.interfaces.TEST_USER_PASSWORD
