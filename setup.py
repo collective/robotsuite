49d6ebdb7fce62894bb30d854181e3c14d5c81c1
Asko Soukka <asko.soukka@iki.fi>
2014-06-29 15:14:46 +0300

    Drop Plone-dependencies to enable Travis-builds for PY3

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 PY3 = sys.version_info[0] == 3
 
 install_requires = [
-        "six",
-        "setuptools",
-        "lxml",
-    ]
+    'six',
+    'setuptools',
+    'lxml'
+]
 
 if PY3:
     install_requires.append('robotframework-python3>=2.8rc1')
@@ -16,31 +16,29 @@ else:
                              'unittest2'])
 
 setup(
-    name="robotsuite",
+    name='robotsuite',
     version='1.5.1.dev0',
-    description="Robot Framework test suite for Python unittest framework",
-    long_description=(open("README.rst").read() + "\n" +
-                      open("CHANGES.txt").read()),
+    description='Robot Framework test suite for Python unittest framework',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.txt').read()),
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
-    keywords="",
-    author="Asko Soukka",
-    author_email="asko.soukka@iki.fi",
-    url="https://github.com/collective/robotsuite/",
-    license="GPL",
-    packages=find_packages("src", exclude=["ez_setup"]),
-    package_dir={"": "src"},
+    keywords='',
+    author='Asko Soukka',
+    author_email='asko.soukka@iki.fi',
+    url='https://github.com/collective/robotsuite/',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
     namespace_packages=[],
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    extras_require={"test": [
-        "plone.app.testing",
-        "robotframework-selenium2library",
+    extras_require={'test': [
     ]}
 )
--- src/robotsuite/tests/test_robot.py
+++ src/robotsuite/tests/test_robot.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 import unittest
-
-from plone.app.testing import PLONE_ZSERVER
-from plone.testing import layered
-
 import robotsuite
 
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("test_plone_login.robot"),
-                layer=PLONE_ZSERVER),
-        robotsuite.RobotTestSuite("test_setups")
+        robotsuite.RobotTestSuite('test_robot.robot'),
+        robotsuite.RobotTestSuite('test_setups')
     ])
     return suite
