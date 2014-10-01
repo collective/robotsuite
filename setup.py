ac3badc7371393e1764e353288cedba572910a27
Asko Soukka <asko.soukka@iki.fi>
2014-10-01 04:55:37 +0300

    Back to development: 1.6.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
 
 setup(
     name='robotsuite',
-    version='1.6.1',
+    version='1.6.2.dev0',
     description='Robot Framework test suite for Python unittest framework',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.txt').read()),
