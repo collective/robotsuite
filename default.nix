2e7be5f54b42d48905b86ec831d8612011a8d951
Asko Soukka <asko.soukka@iki.fi>
2021-05-15 20:25:24 +0300

    Migrate to github-actions

---
+++ default.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import ./nix { nixpkgs = sources."nixpkgs-20.09"; }
+, sources ? import ./nix/sources.nix {}
+, python ? "python39"
+}:
+
+(import ./setup.nix { inherit pkgs sources python; }).package
--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
-import sys
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-PY3 = sys.version_info[0] == 3
-
-install_requires = [
-    'six',
-    'setuptools',
-    'lxml',
-    'robotframework>=2.8',
-]
 
 setup(
-    name='robotsuite',
-    version='2.2.2.dev0',
-    description='Robot Framework test suite for Python unittest framework',
-    long_description=(open('README.rst').read() + '\n' +
-                      open('CHANGES.txt').read()),
-    # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    keywords='',
-    author='Asko Soukka',
-    author_email='asko.soukka@iki.fi',
-    url='https://github.com/collective/robotsuite/',
-    license='GPL',
-    packages=find_packages('src', exclude=['ez_setup']),
-    package_dir={'': 'src'},
-    namespace_packages=[],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    extras_require={'test': [
-    ]}
+    # thanks to this bug
+    # https://github.com/pypa/setuptools/issues/1136
+    # we need one line in here:
+    package_dir={'': 'src'}
 )
